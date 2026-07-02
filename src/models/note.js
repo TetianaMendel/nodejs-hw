5aaaf2ee98a1d230c05a9ea575a19cb7a2537d86
TetianaMendel <tanyabulasvska@ukr.net>
2026-07-02 22:08:15 +0300

    Fix Note schema validation

--- src/models/note.js
+++ src/models/note.js
@@ -9,13 +9,11 @@ const noteSchema = new Schema(
     },
     content: {
       type: String,
-      required: false,
-      default: " ",
+      default: '',
       trim: true,
     },
     tag: {
       type: String,
-      required: true,
       enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
       default: 'Todo'
     },
